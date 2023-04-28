e78f99b33782d4a39184a132a58fccc6a5575d74
snrp2002 <snrp2002@gmail.com>
2023-04-29 00:00:32 +0530

    Modification1

--- src/ImageInput.js
+++ src/ImageInput.js
@@ -2,14 +2,48 @@ import React, { useState } from "react";
 
 export default function ImageInput()
 {
-    const [file, setFile] = useState();
-    function handleChange(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
+    const [file, setFile] = useState("");
+    const convertToBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
+    const handleChange = async(e) => {
+        if(e.target.files.length === 0){
+            setFile("");
+            return;
+        }
+        const file = e.target.files[0];
+        const base64 = await convertToBase64(file);
+        setFile(base64);
     }
-     const handleSubmit=(e)=>{
+    const handleSubmit= async(e)=>{
         e.preventDefault();
-     }
+        try{
+            const response = await fetch("http://localhost:5000/", {
+                method: 'POST',
+                headers:{
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({file : file})
+            });
+            const data = await response.json();
+            if(response.status !== 200){
+                throw data;
+            }else{
+                console.log(data.output);
+            }
+        }catch(err){
+            console.log("Error: " + err.message);
+        }
+    }
 
     return <form onSubmit={handleSubmit} className="form">
         <label>Upload an image of single digit</label>
